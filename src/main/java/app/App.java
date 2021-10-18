0ac1e739a8413d10d7cde03677cbaeaa7296760a
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-18 10:59:58 +0800

    parsing of User.json

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,16 @@
 package app;
 
 import model.User;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 
 public class App {
@@ -11,6 +18,38 @@ public class App {
     static Map<String, User> userIdMap = new HashMap<>();
 
     public static void main(String[] args) {
-
+        populateUserIdMap();
     }
+
+    public static void populateUserIdMap() {
+        JSONParser parser = new JSONParser();
+
+        try ( BufferedReader br = new BufferedReader(new FileReader("data/yelp_academic_dataset_user.json"))){
+            String line = br.readLine();
+            while (line != null) {
+                JSONObject jsonObject = (JSONObject) parser.parse(line);
+                String user_id = (String) jsonObject.get("user_id");
+                List<String> friends = new ArrayList<>();
+
+                String friendsString = (String) jsonObject.get("friends");
+
+                for (String friend: friendsString.split(",")) {
+                    friends.add(friend.strip());
+                }
+                User user = new User(user_id, friends);
+
+                userIdMap.put(user_id, user);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module yelpfriends {
+    requires json.simple;
+}

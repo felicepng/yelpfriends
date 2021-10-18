2a1d726b5fff53e59b0c2f907149dc4e21668e00
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-18 09:58:19 +0800

    convert to Maven project

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,15 +1,16 @@
 package app;
 
-import model.*;
+import model.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class App {
-    
+
     static Map<String, User> userIdMap = new HashMap<>();
+
     public static void main(String[] args) {
-        
+
     }
 }

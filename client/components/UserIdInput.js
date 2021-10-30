08eb1fbf5950de9d52b34ed75e784b890443caea
Felice <felicepng2010@gmail.com>
2021-10-30 23:52:51 +0800

    Add table for data buttons

--- client/components/UserIdInput.js
+++ client/components/UserIdInput.js
@@ -8,9 +8,9 @@ const UserIdInput = (props) => {
 
     return (
         <div className="">
-            <p>User Id: {userId}</p>
-            <input className="border" value={tempId} onChange={(e) => {setTempId(e.target.value)}}/>
-            <button className="border px-2 ml-4" onClick={()=>{setUserId(tempId)}}>Log in</button>
+            {/* <p>User Id: {userId}</p> */}
+            <input className="focus:outline-none border rounded-full px-6 py-2 w-96 shadow" placeholder="Enter your user ID" value={tempId} onChange={(e) => {setTempId(e.target.value)}}/>
+            {/* <button className="border px-2 ml-4" onClick={()=>{setUserId(tempId)}}>Log in</button> */}
         </div>
     )
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -18,13 +18,89 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <div className="w-screen h-80 justify-between flex items-center flex-col">
-        <h1 className="text-2xl font-bold">Hello, welcome to YelpFriends</h1>
-        <LoadData />
-        <UserIdInput {...{userId, setUserId}}/>
-        <AdjacencyMatrix {...{userId}}/>
-        <AdjacencyList {...{userId}}/>
+      <div className="w-screen justify-between flex items-center flex-col">
+        <div className="w-screen h-40 flex flex-col items-center justify-center bg-red-50">
+          <div className="mb-4 gap-x-1 flex items-center">
+            <img src="/logo.png" className="h-11" />
+            <h1 className="text-4xl font-bold">friends</h1>
+          </div>
+
+          {/* <LoadData /> */}
+          <UserIdInput {...{ userId, setUserId }} />
         </div>
+
+        {/* <AdjacencyMatrix {...{ userId }} />
+        <AdjacencyList {...{ userId }} /> */}
+        <div className="p-8 w-screen">
+          <div className="grid grid-cols-4 gap-x-8">
+            <div className="p-4 bg-gray-50 rounded-3xl col-span-2 grid grid-rows-4 gap-y-4">
+              <div className="grid grid-cols-4 gap-x-4">
+                <div />
+                <div className="px-4 text-center">
+                  Populate data
+                </div>
+                <div className="px-4 text-center">
+                  Get 1st degree friends
+                </div>
+                <div className="px-4 text-center">
+                  Get 2nd degree friends
+                </div>
+              </div>
+              <div className="grid grid-cols-4 gap-x-4">
+                <div>
+                  Using Adjacency Matrix
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+              </div>
+              <div className="grid grid-cols-4 gap-x-4">
+                <div>
+                  Using Adjacency List
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+              </div>
+              <div className="grid grid-cols-4 gap-x-4">
+                <div>
+                  Using Tree
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+                <div className="bg-white rounded-2xl">
+                  Button
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-gray-50 rounded-3xl col-span-1">
+              1st Degree Friends
+            </div>
+
+            <div className="bg-gray-50 rounded-3xl col-span-1">
+              2nd Degree Friends
+            </div>
+
+          </div>
+        </div>
+      </div>
 
     </div>
   )

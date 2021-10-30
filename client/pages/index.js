129304a4fc350268c747268ad62915d5030f98fc
Felice <felicepng2010@gmail.com>
2021-10-31 00:17:32 +0800

    Edit table buttons

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,6 +5,7 @@ import AdjacencyList from '../components/AdjacencyList'
 import AdjacencyMatrix from '../components/AdjacencyMatrix'
 import LoadData from '../components/LoadData'
 import UserIdInput from '../components/UserIdInput'
+// import { AiFillPlayCircle } from 'react-icons/ai'
 
 export default function Home() {
 
@@ -33,61 +34,72 @@ export default function Home() {
         <AdjacencyList {...{ userId }} /> */}
         <div className="p-8 w-screen">
           <div className="grid grid-cols-4 gap-x-8">
-            <div className="p-4 bg-gray-50 rounded-3xl col-span-2 grid grid-rows-4 gap-y-4">
-              <div className="grid grid-cols-4 gap-x-4">
+            <div className="p-4 bg-gray-50 rounded-3xl col-span-2 gap-y-4">
+              <div className="grid grid-cols-4 gap-x-4 mb-3">
                 <div />
-                <div className="px-4 text-center">
+                <div className="text-sm text-gray-400 font-semibold px-4 text-center">
                   Populate data
                 </div>
-                <div className="px-4 text-center">
+                <div className="text-sm text-gray-400 font-semibold px-4 text-center">
                   Get 1st degree friends
                 </div>
-                <div className="px-4 text-center">
+                <div className="text-sm text-gray-400 font-semibold px-4 text-center">
                   Get 2nd degree friends
                 </div>
               </div>
-              <div className="grid grid-cols-4 gap-x-4">
-                <div>
-                  Using Adjacency Matrix
+              <div className="grid grid-rows-3 gap-y-4">
+                <div className="h-20 grid grid-cols-4 gap-x-4">
+                  <div className="flex items-center pr-12 text-sm text-gray-500 font-semibold">
+                    Using Adjacency Matrix
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
                 </div>
-                <div className="bg-white rounded-2xl">
-                  Button
+                <div className="grid grid-cols-4 gap-x-4">
+                  <div className="flex items-center pr-12 text-sm text-gray-500 font-semibold">
+                    Using Adjacency List
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
                 </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
+                <div className="grid grid-cols-4 gap-x-4">
+                  <div className="flex items-center text-sm text-gray-500 font-semibold">
+                    Using Tree
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
+                  <div className="flex items-center justify-center bg-white rounded-2xl">
+                    Button
+                  </div>
                 </div>
               </div>
-              <div className="grid grid-cols-4 gap-x-4">
-                <div>
-                  Using Adjacency List
+
+              <div className="border-t-2 pt-4 mt-4 grid grid-cols-4 gap-x-4">
+                  <div className="flex items-center text-sm text-gray-500 font-semibold">
+                    Fastest
+                  </div>
+                  <div className="h-14 flex items-center justify-center bg-white rounded-2xl" />
+                  <div className="flex items-center justify-center bg-white rounded-2xl" />
+                  <div className="flex items-center justify-center bg-white rounded-2xl" />
                 </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-              </div>
-              <div className="grid grid-cols-4 gap-x-4">
-                <div>
-                  Using Tree
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-                <div className="bg-white rounded-2xl">
-                  Button
-                </div>
-              </div>
             </div>
 
             <div className="bg-gray-50 rounded-3xl col-span-1">

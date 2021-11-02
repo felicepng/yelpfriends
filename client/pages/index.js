b101c542a5e82a207bc83e5226e689b07ac1e14c
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-02 09:02:03 +0800

    instructions

--- client/pages/index.js
+++ client/pages/index.js
@@ -31,11 +31,17 @@ export default function Home() {
       </Head>
 
       <div className="w-screen justify-between flex items-center flex-col">
-        <Modal title="How to use" visible={isModalOpen} centered closeable footer={null} onOk={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)}>
-          <div>Instructions...</div>
-          <div>Instructions...</div>
-          <div>Instructions...</div>
-          <div>Instructions...</div>
+        <Modal width={800} visible={isModalOpen}  centered closeable footer={null} onOk={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)}>
+          <div className="grid grid-cols-7">
+            <img src="/instructions.webp" className="h-72 my-auto col-span-3" />
+            <div className="m-5 col-span-4">
+              <div className="text-5xl font-bold text-gray-600 mb-3">Instructions</div>
+              <h1 className="text-gray-500">Welcome to YelpFriends! Here are some instructions to take note while using our app:</h1>
+              <h1 className="text-gray-500">1. Do key in your user id and click on the log in button. We have provided a default user id. Should you change the user id, do make sure it a valid one.</h1>
+              <h1 className="text-gray-500">2. You will need to load the data for each of the data structures before proceeding to find your 1st and 2nd degree friends.</h1>
+              <h1 className="text-gray-500">3. Please find your first degree friends before finding your second degree friends</h1>
+            </div>
+          </div>
         </Modal>
         <div className="w-screen h-40 flex flex-col items-center justify-center bg-theme">
           <div className="mb-4 flex justify-center items-center gap-x-4">

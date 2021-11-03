af5e4b9a4eacf2071e618164233f67db91a5d49d
Felice <felicepng2010@gmail.com>
2021-11-03 18:39:34 +0800

    Edit instructions modal

--- client/components/InstructionsModal.js
+++ client/components/InstructionsModal.js
@@ -10,11 +10,11 @@ const InstructionsModal = (props) => {
             <div className="grid grid-cols-7">
                 <img src="/instructions.webp" className="h-72 my-auto col-span-3" />
                 <div className="m-5 col-span-4">
-                    <div className="text-5xl font-bold text-gray-600 mb-3">Instructions</div>
+                    <div className="text-4xl font-bold text-gray-600 mb-3">Instructions</div>
                     <h1 className="text-gray-500">Welcome to YelpFriends! Here are some instructions to take note while using our app:</h1>
-                    <h1 className="text-gray-500">1. We have provided a default user id. Should you decide to change it, key in your desired user id and click on the log in button but make sure it is valid and in range. </h1>
-                    <h1 className="text-gray-500">2. You will need to load the data for each of the data structures before proceeding to find your 1st and 2nd degree friends. We have hardcoded a value of 15,000 users to be loaded but this can be changed in the backend.</h1>
-                    <h1 className="text-gray-500">3. Please find your first degree friends before finding your second degree friends</h1>
+                    <h1 className="text-gray-500 mt-6">1. We have provided a <span className="text-green-700">default user ID.</span> Should you wish to change it, key in your desired user ID and click on the log in button. Make sure the ID is <span className="text-green-700">valid and in range.</span></h1>
+                    <h1 className="text-gray-500">2. You will need to <span className="text-green-700">load the data</span> for each of the data structures before proceeding to find your 1st and 2nd degree friends. We have hardcoded a value of 15,000 users to be loaded but this can be changed in the backend.</h1>
+                    <h1 className="text-gray-500">3. Please find your 1st degree friends <span className="text-green-700">before</span> finding your 2nd degree friends.</h1>
                 </div>
             </div>
         </Modal>

018b02a9366ee37336e3aee82d8292a543a9cbe8
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-03 17:15:47 +0800

    comments for adj list

--- src/main/java/com/example/YelpFriends/model/AdjacencyList.java
+++ src/main/java/com/example/YelpFriends/model/AdjacencyList.java
@@ -17,17 +17,26 @@ public class AdjacencyList {
 
     public void buildAdjacencyList() {
         long startTime = System.nanoTime();
+
+        // Create map to link user_id to list of friends' user_ids
         HashMap<String,ArrayList<String>> adjList = new HashMap<>();
+
+        // Get list of users in database
         List<User> users = userRepository.findAll();
 
+        // Iterate through list
         for (User user : users){
+            // Get set of user's friends' user_ids
             Set<String> allUserFriends = user.getFriends();
+
             ArrayList<String> existingUserFriends = new ArrayList<>();
             for (String user_ID: allUserFriends){
+                // Add friend to list if friend exists in database
                 if (userRepository.existsByUserId(user_ID)){
                     existingUserFriends.add(user_ID);
                 }
             }
+            // Put user and user's friends list in map
             adjList.put(user.getUserId(),new ArrayList<>(existingUserFriends));
         }
 
@@ -36,6 +45,7 @@ public class AdjacencyList {
         System.out.println("Time taken to build an Adjacency List is " + (endTime - startTime) + "ns");
     }
 
+    // for debugging purposes
     public void printAdjacencyList() {
         System.out.println("Adjacency List:");
         for (Map.Entry<String, ArrayList<String>> entry : fullAdjacencyList.entrySet()) {
@@ -72,33 +82,49 @@ public class AdjacencyList {
         fullAdjacencyList.get(user2Id).remove(user1Id);
     }
     
+    // Get first degree friends using Adj List
     public List<String> getFirstDegree(String userId) {
         long startTime = System.nanoTime();
+        // Get list of user's friends from map
         ArrayList<String> toBeReturned = fullAdjacencyList.get(userId);
         long endTime = System.nanoTime();
         System.out.println("Time taken to get first degree from Adjacency List is " + (endTime - startTime) + "ns");
         return toBeReturned;
     }
 
+    // Get second degree friends using Adj List
     public Map<String, Integer> getSecondDegree(String userId) {
         long startTime = System.nanoTime();
+
+        // Make new map to store all second degree connections
         HashMap<String,Integer> secondDegreeConnections = new HashMap<>();
+
+        // Get list of user's friends from map
         List<String> firstDegree = fullAdjacencyList.get(userId);
 
-
+        // Iterate through first degree friends
         for (String firstDegFriend : firstDegree) {
             Optional<User>firstDegreeFriend  = userRepository.findByUserId(firstDegFriend);
             if (firstDegreeFriend.isEmpty()){
                 continue;
             }
+
+            // Set of first degree friend's first degree connections, 
+            // i.e. selected user's second degree connections
             Set<String> secondDegree = firstDegreeFriend.get().getFriends();
 
             for (String secondDegFriend : secondDegree) {
+                // Check if:
+                // 1) secondDegFriend is already a first Degree friend
+                // 2) secondDegFriend is the selected user
+                // 3) secondDegFriend exists in the database
                 if (!firstDegree.contains(secondDegFriend) && !userId.equals(secondDegFriend) && userRepository.existsByUserId(secondDegFriend)) {
+                    // if secondDegFriend exists in map, increase mutual friend count by 1
                     if (secondDegreeConnections.containsKey(secondDegFriend)){
                         Integer numberOfMutualFriends = secondDegreeConnections.get(secondDegFriend);
                         secondDegreeConnections.put(secondDegFriend,numberOfMutualFriends+1);
                     }
+                    // else initialise mutual friend count to 1
                     else{
                         secondDegreeConnections.put(secondDegFriend,1);
                     }

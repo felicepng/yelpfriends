688b58f33f1067b9cc18ee5b0ef823513244f202
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-03 13:09:38 +0800

    changed instructions text

--- client/components/InstructionsModal.js
+++ client/components/InstructionsModal.js
@@ -12,8 +12,8 @@ const InstructionsModal = (props) => {
                 <div className="m-5 col-span-4">
                     <div className="text-5xl font-bold text-gray-600 mb-3">Instructions</div>
                     <h1 className="text-gray-500">Welcome to YelpFriends! Here are some instructions to take note while using our app:</h1>
-                    <h1 className="text-gray-500">1. Do key in your user id and click on the log in button. We have provided a default user id. Should you change the user id, do make sure it a valid one.</h1>
-                    <h1 className="text-gray-500">2. You will need to load the data for each of the data structures before proceeding to find your 1st and 2nd degree friends.</h1>
+                    <h1 className="text-gray-500">1. We have provided a default user id. Should you decide to change it, key in your desired user id and click on the log in button but make sure it is valid and in range. </h1>
+                    <h1 className="text-gray-500">2. You will need to load the data for each of the data structures before proceeding to find your 1st and 2nd degree friends. We have hardcoded a value of 15,000 users to be loaded but this can be changed in the backend.</h1>
                     <h1 className="text-gray-500">3. Please find your first degree friends before finding your second degree friends</h1>
                 </div>
             </div>
